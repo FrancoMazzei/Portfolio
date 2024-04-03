882c4fdf71754f0ac93cb00e92c7955ab2bfc876
FrancoMazzei <martinrivadaa@gmail.com>
2024-04-03 14:09:41 -0300

    ke onda pe

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,8 @@ export default {
 		extend: {
 			backgroundImage: {
 				'gradient-hero': 'linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.78) 100%), url("/public/img/Cas6.webp")',
-			},
-			
-
+				'fom-mi-hero': 'linear-gradient(0deg, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.7) 54%, rgba(0,0,0,0.9) 87%, rgba(0,0,0,0.9) 100%), url("/public/img/Fom.webp")',
+			},	
 		},
 	},
 	plugins: [],
