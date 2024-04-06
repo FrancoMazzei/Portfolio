ac9d6b327157dae93e8409c4c730db02e3158824
FrancoMazzei <martinrivadaa@gmail.com>
2024-04-05 22:57:17 -0300

    colta

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,20 @@ export default {
 				'gradient-hero': 'linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.78) 100%), url("/public/img/Cas6.webp")',
 				'fom-mi-hero': 'linear-gradient(0deg, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.7) 54%, rgba(0,0,0,0.9) 87%, rgba(0,0,0,0.9) 100%), url("/public/img/Fom.webp")',
 			},	
+			keyframes: {
+				borderMove: {
+				'0%': { transform: 'rotate(0deg)' },
+				'100%': { transform: 'rotate(360deg)' },
+				},
+			},
+			colors: {
+				'custom-purple': {
+				'500': 'rgb(173, 115, 166)',
+				}, 
+			},
+			
+
+			
 		},
 	},
 	plugins: [],
