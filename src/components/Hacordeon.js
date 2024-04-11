e772d5227721670367a3583d36bd476e817848c8
FrancoMazzei <martinrivadaa@gmail.com>
2024-04-11 17:18:46 -0300

    violeta como la marihuana pe

--- src/components/Hacordeon.js
+++ src/components/Hacordeon.js
@@ -1,11 +1,83 @@
+// let questions = document.querySelectorAll(".faq_question");
+
+//     questions.forEach((question) => {
+//         let icon = question.querySelector(".icon-shape");
+
+//         question.addEventListener("click", (event) => {
+//             const active = document.querySelector(".faq_question.active");
+//             const activeIcon = document.querySelector(".icon-shape.active");
+
+//         if (active && active !== question) {
+//             active.classList.toggle("active");
+//             activeIcon.classList.toggle("active");
+//             active.nextElementSibling.style.maxHeight = 0;
+//         }
+
+//             question.classList.toggle("active");
+//             icon.classList.toggle("active");
+
+//         const answer = question.nextElementSibling;
+
+//         if (question.classList.contains("active")) {
+//             answer.style.maxHeight = answer.scrollHeight + "px";
+//         } else {
+//             answer.style.maxHeight = 0;
+//         }
+//         });
+//     });
+
+
+
+// let questions = document.querySelectorAll(".faq_question");
+
+// questions.forEach((question) => {
+//     let icon = question.querySelector(".icon-shape");
+
+//     question.addEventListener("click", (event) => {
+
+//         let backgroundImage = document.getElementById("Herramientas");
+
+//         if (question.classList.contains("active")) {
+            
+//             backgroundImage.style.backgroundPosition = "center top";
+//         } else {
+            
+//             backgroundImage.style.backgroundPosition = "center center";
+//         }
+
+//         const active = document.querySelector(".faq_question.active");
+//         const activeIcon = document.querySelector(".icon-shape.active");
+
+//         if (active && active !== question) {
+//             active.classList.toggle("active");
+//             activeIcon.classList.toggle("active");
+//             active.nextElementSibling.style.maxHeight = 0;
+//         }
+
+//         question.classList.toggle("active");
+//         icon.classList.toggle("active");
+
+//         const answer = question.nextElementSibling;
+
+//         if (question.classList.contains("active")) {
+//             answer.style.maxHeight = answer.scrollHeight + "px";
+//         } else {
+//             answer.style.maxHeight = 0;
+//         }
+//     });
+// });
+
+
 let questions = document.querySelectorAll(".faq_question");
 
-    questions.forEach((question) => {
-        let icon = question.querySelector(".icon-shape");
+questions.forEach((question) => {
+    let icon = question.querySelector(".icon-shape");
 
-        question.addEventListener("click", (event) => {
-            const active = document.querySelector(".faq_question.active");
-            const activeIcon = document.querySelector(".icon-shape.active");
+    question.addEventListener("click", (event) => {
+        event.stopPropagation(); 
+        
+        const active = document.querySelector(".faq_question.active");
+        const activeIcon = document.querySelector(".icon-shape.active");
 
         if (active && active !== question) {
             active.classList.toggle("active");
@@ -13,8 +85,8 @@ let questions = document.querySelectorAll(".faq_question");
             active.nextElementSibling.style.maxHeight = 0;
         }
 
-            question.classList.toggle("active");
-            icon.classList.toggle("active");
+        question.classList.toggle("active");
+        icon.classList.toggle("active");
 
         const answer = question.nextElementSibling;
 
@@ -23,5 +95,5 @@ let questions = document.querySelectorAll(".faq_question");
         } else {
             answer.style.maxHeight = 0;
         }
-        });
     });
+});
