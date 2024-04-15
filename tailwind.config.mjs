1453231a45727e1c594e545133e59764df83580c
FrancoMazzei <martinrivadaa@gmail.com>
2024-04-15 20:41:39 -0300

    cabeza pinga

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,8 @@ export default {
 			backgroundImage: {
 				'gradient-hero': 'linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.78) 100%), url("/public/img/Cas6.webp")',
 				'fom-mi-hero': 'linear-gradient(0deg, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.7) 54%, rgba(0,0,0,0.9) 87%, rgba(0,0,0,0.9) 100%), url("/public/img/Fom.webp")',
-				'her-hero': 'linear-gradient(0deg, rgba(0,0,0,0.95) 0%, rgba(0,0,0,0.95) 27%, rgba(0,0,0,0.95) 77%, rgba(0,0,0,0.95) 100%), url("/public/img/F3-4.jpg")',
+				'her-hero': 'linear-gradient(0deg, rgba(0,0,0,0.95) 0%, rgba(0,0,0,0.95) 27%, rgba(0,0,0,0.95) 77%, rgba(0,0,0,0.95) 100%), url("/public/img/F3-4.webp")',
+				'proyec-hero':'linear-gradient(90deg, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0.8) 9%, rgba(0,0,0,0.8) 91%, rgba(0,0,0,1) 100%), url("/public/img/FP.webp")',
 			},	
 			keyframes: {
 				borderMove: {
@@ -19,6 +20,7 @@ export default {
 				'500': 'rgb(173, 115, 166)',
 				}, 
 				customOrange: '#ff6f00',
+				
 			},
 			
 			
